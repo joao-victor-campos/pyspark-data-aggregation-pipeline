fd72cb2c7d3073520b4d89d6a0443ae46a2a6022
João Victor Campos Alves Moraes <mmoraes.victor@gmail.com>
2022-06-28 23:06:15 -0300

    lint fixes

--- spark_pipeline/base/spark_client.py
+++ spark_pipeline/base/spark_client.py
@@ -5,11 +5,8 @@ from pyspark.sql import SparkSession
 #     def __init__(self, session: Optional[SparkSession] = None) -> None:
 #         self._session = session
 
-def CreateSession() -> SparkSession:
-    #if not self._session: 
-        session = SparkSession \
-            .builder \
-            .appName("aggregation") \
-            .getOrCreate()
-        return session
 
+def CreateSession() -> SparkSession:
+    # if not self._session:
+    session = SparkSession.builder.appName("aggregation").getOrCreate()
+    return session
--- tests/unit/spark_pipeline/test_sparkclient.py
+++ tests/unit/spark_pipeline/test_sparkclient.py
@@ -1,5 +1,7 @@
-from spark_pipeline.base.spark_client import CreateSession
 from pyspark.sql import SparkSession
+
+from spark_pipeline.base.spark_client import CreateSession
+
 
 def test_spark_client():
     assert type(CreateSession()) == SparkSession
