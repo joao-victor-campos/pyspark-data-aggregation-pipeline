5b07149a09b122ae1f536a0e465a9f34605372a7
João Victor Campos Alves Moraes <mmoraes.victor@gmail.com>
2022-07-02 23:28:37 -0300

    create tests for death_per_year aggreagation

--- tests/unit/spark_testes/test_sparkclient.py
+++ tests/unit/spark_testes/test_sparkclient.py
@@ -1,7 +1,20 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import DataFrame, SparkSession
 
 from spark_pipeline.base.spark_client import CreateSession
+from spark_pipeline.deaths_per_year import extract, load, transform
 
 
 def test_spark_client():
     assert type(CreateSession()) == SparkSession
+
+
+def test_extract():
+    assert type(extract()) == DataFrame
+
+
+def test_transform():
+    assert type(transform(extract())) == DataFrame
+
+
+def test_load():
+    assert type(load(transform(extract()))) == None
