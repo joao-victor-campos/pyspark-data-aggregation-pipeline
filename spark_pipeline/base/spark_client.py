7daf41d428743523fefa10f85bb3325a303bba22
Nicholas Baraldi <64378761+NicholasBaraldi@users.noreply.github.com>
2022-07-03 22:49:55 -0300

    Refactoring Spark client and pipeline (#9)

--- spark_pipeline/base/spark_client.py
+++ spark_pipeline/base/spark_client.py
@@ -1,16 +1,30 @@
+from typing import Optional
+
 from pyspark.sql import SparkSession
 
-# class SparkClient:
-#     def __init__(self, session: Optional[SparkSession] = None) -> None:
-#         self._session = session
 
+class SparkClient:
+    """Handle the creation of a Spark session."""
 
-def CreateSession() -> SparkSession:
-    """Creates a Spark Session.
+    def __init__(self, session: Optional[SparkSession] = None) -> None:
+        self._session = session
 
-    Returns:
-        Spark session.
-    """
-    # if not self._session:
-    session = SparkSession.builder.appName("aggregation").getOrCreate()
-    return session
+    def Session(self) -> SparkSession:
+        """Get a created SparkSession or if it doesn't exist create one.
+
+        Returns:
+            Spark session.
+        """
+        if not self._session:
+            self._session = self.CreateSession()
+        return self._session
+
+    def CreateSession(self) -> SparkSession:
+        """Creates a Spark Session.
+
+        Returns:
+            Spark session.
+        """
+        # if not self._session:
+        session = SparkSession.builder.getOrCreate()
+        return session
--- spark_pipeline/deaths_per_year.py
+++ spark_pipeline/deaths_per_year.py
@@ -1,6 +1,7 @@
 from pyspark.sql import DataFrame, functions
 
-from spark_pipeline.base.spark_client import CreateSession
+from spark_pipeline.base.read_write import read_csv, write_csv
+from spark_pipeline.base.spark_client import SparkClient
 
 QUERY = """
 SELECT
@@ -18,13 +19,8 @@ def extract() -> DataFrame:
     Returns:
         Spark DataFrame
     """
-    CreateSession().read.load(
-        "data/AgeDataset-V1.csv",
-        format="csv",
-        inferSchema="true",
-        header="true",
-    ).createOrReplaceTempView("deaths")
-    return CreateSession().sql(QUERY)
+    read_csv("data/AgeDataset-V1.csv", header="true").createOrReplaceTempView("deaths")
+    return SparkClient().Session().sql(QUERY)
 
 
 def transform(df: DataFrame) -> DataFrame:
@@ -36,9 +32,7 @@ def transform(df: DataFrame) -> DataFrame:
     Returns:
         DataFrame: Aggregated DataFrame
     """
-    df_blank = df.fillna("Blank", subset=["Death_year"]).select(
-        functions.col("Id"), functions.col("Death_year").cast("int")
-    )
+    df_blank = df.select(functions.col("Id"), functions.col("Death_year").cast("int"))
 
     agg_df = (
         df_blank.groupBy("Death_year")
@@ -55,5 +49,5 @@ def load(df: DataFrame) -> None:
     Args:
         df (DataFrame): Aggregated DataFrame
     """
-    df.coalesce(1).write.parquet("data/output", mode="overwrite")
+    write_csv(df, "data/output")
     return None
